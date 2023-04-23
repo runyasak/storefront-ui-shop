884090da32f6a95ad4fad4071d53d4cbeaf5581d
Runyasak Chaengnaimuang <world@odds.team>
2023-04-23 23:49:23 +0700

    feat: add tailwind and storefront-ui

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <SfButton type="button" class="w-full"> Hello </SfButton>
 </template>
+
+<script lang="ts" setup>
+import { SfButton } from "@storefront-ui/vue";
+</script>
